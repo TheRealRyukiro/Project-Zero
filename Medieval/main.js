77ec26212b118835b0cd99727d08d38228626cfe
TheRealRyukiro <tomdunbar2000@gmail.com>
2021-08-23 23:47:57 -0400

    Co-authored-by: Durkovic, Nathaniel <NDurkovic@eeeug.com>

--- Medieval/main.js
+++ Medieval/main.js
@@ -1,5 +1,8 @@
 var playerName;
 
 
-
-function submitName(){playerName = document.getElementById("nameInput").value;}
+document.getElementById("nameInputButton").addEventListener("click", function(){
+    playerName = document.getElementById("nameInput").value;
+    document.getElementById("Name").innerHTML = playerName;
+    document.getElementById("overlay").remove();
+});
